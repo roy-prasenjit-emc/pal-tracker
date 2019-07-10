da97074d5f696a45ee0ff12e1becf4fb8a98d978
PAL-3 <Prasenjit Roy>
2019-07-10 16:15:10 +0530

    update repo

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,5 +1,7 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,9 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository(@Value("${spring.datasource.url:NOT SET}") String url) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(url);
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
